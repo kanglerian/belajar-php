4051a150c190bbcf028a1f6fe1bd4b20e727f29c
kanglerian <kanglerian@gmail.com>
2020-12-17 20:33:22 +0700

    pertemuan 3

---
+++ pertemuan-3/latihan1.php
@@ -0,0 +1,26 @@
+<?php
+// Pengulangan ada 4 jenis
+// for, while, do .. while, foreach : khusus array
+
+// for ($i = 0; $i < 5; $i++) {
+//     echo "Hello World! <br>";
+// }
+
+// $i = 0;
+// while ($i < 5) {
+//     echo "Hello World <br>";
+//     $i++;
+// }
+
+
+// $i = 10;
+// do {
+//     echo "hello world <br>";
+//     $i++;0
+// } while ($i < 5);
+
+// $i = 10;
+// while ($i < 5) {
+//     echo "hello world <br>";
+//     $i++;
+// }
---
+++ pertemuan-3/latihan4.php
@@ -0,0 +1,13 @@
+<?php
+// Pengkondisian / Percabangan
+// if else, if elseif else, ternary, switch
+
+$x = 20;
+
+if ($x < 20) {
+    echo "Benar";
+} elseif ($x == 20) {
+    echo "Bingo!";
+} else {
+    echo "salah";
+}
